533fc3d1189788396a8a1d318f1313adf4493a22
Ussama Naal <usa.naal@gmail.com>
2021-03-08 00:36:55 +0000

    render ground white color

--- src/environment.cpp
+++ src/environment.cpp
@@ -56,9 +56,10 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
       pointProcessor.SegmentPlane(cloud, 100, 0.2);
   
     // renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
-    // renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(1,1,1));
   
-  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
+  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters =
+    pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
 
 	int clusterId = 0;
 	std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1)};
