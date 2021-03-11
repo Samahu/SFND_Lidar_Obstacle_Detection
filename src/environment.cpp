6a8ab5b019ab496f6a5bb4e1f62a633c15a35629
Ussama Naal <usa.naal@gmail.com>
2021-03-11 05:52:44 +0000

    add bounding boxes

--- src/environment.cpp
+++ src/environment.cpp
@@ -69,6 +69,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
       std::cout << "cluster size ";
       pointProcessor.numPoints(cluster);
       renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
+      auto box = pointProcessor.BoundingBox(cluster);
+      renderBox(viewer, box, clusterId);
       ++clusterId;
 	}
   
